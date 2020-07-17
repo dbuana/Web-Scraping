f45d8f8641e6224b64410f893152bd32ac810517
dbuana <46995714+dbuana@users.noreply.github.com>
2020-07-17 12:39:02 +0900

    1/2 of the big 4
    As 1/2 of the big four, Amazon, is a part of the big four company. This code includes the Amazon web scraping code; however, the code is not complete, and the code is also basic. This code deserves a potential of sophistication, furthermore, the code will be frequently updated with every chance it has.
    
    Attachments: BeautifulSoup4 and Python3

--- Amazon/theBigFour.py
+++ Amazon/theBigFour.py
@@ -1,67 +1,29 @@
-# This tab is mainly used to scrap data from the big 4 company.
-# 1). Google inc.
-# 2). FaceBook
-# 3). Amazon
-# 4). Apple inc. 
+def amazonData(url):
+	# The main code; it will be the most basic section of the whole program, or at least, the most
+	# similar compared to the codes written above. 
+	#AmaUserItem = input("Type in an item: ") # Item search Amazon
 
-# The Google data scrap
-# Task: Give a user accesibilty to go anywhere, and find data; in form of HTML. In any companies
-# that posseses correlation with Google; like YouTube, Gmail and etc. Good luck!
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
-from bs4 import BeautifulSoup
-
-# Custom modules and libraries for the program
-def googleUrl(url):
-	return url
-
-def log_error(a):
-	print(a)
-
-# The module consists of errors, attachments and many more. It also 
-# includes alternative
-def google_error(e):
-	print(e)
-
-def invalid_error(x):
-	print(x)
-
-def googleData():
-	"""
-	Includes a user-interface and then, it also includes
-	Google's data in form of HTML format. Interestingly, it should also
-	inlclude other companies data; with correlation with Google. 
-	"""
-	# The main contribution is a user-interface
-	user = input("Which Google owned website would you like to visit: ")
-
-	try:
-		with closing(get(url, stream=True)) as resp:
-			if closing(resp):
-				return resp.content
-			else:
-				return None
-	# When typing an incoherrent URL, it returns a "log error"; which means
-	# the program has a difficulty on comprehensing the URL and it's data.
-	except RequestException as a:
-		log_error("Invalid website {0} and {1}" + str(a) + url)
+	amazonUrl =  "https://www.amazon.co.jp/b/?ie=UTF8&language=en_US&node=1094656"
+	
+	if url != amazonUrl:
+		search_error("Webpage not found, check your URL once again, then we may assist you to your site. {0} and {1}" + str(I) + url + " is invalid")
 		return None
-	# When typing an URL, that is unknown to any domain. It returns an invalid 
-	# error. It notifies the user that the website is unknown to any server. 
-	if RequestException == x:
-		invalid_error("The URL is invalid {0} and {1}" + url + str(x))
-
-
-	# The statement will be a mojor aspect
-	if user == "youtube" or user == "YouTube":
-		googleUrl("https://www.google.com/")
-		return ("You may be assisted to {0} and {1}" + resp.content("https://www.google.com/"))
-	elif user == "gmail" or user == "Gmail":
-		googleUrl("https://www.google.com/gmail/about/#")
-		return ("you may be assisted to {0} and {1}" + googleUrl("https://www.google.com/gmail/about/#") + resp.content("https://www.google.com/gmail/about/#"))
+	elif url == amazonUrl:
+		return ("You may be assisted to " + resp.content( "https://www.amazon.co.jp/b/?ie=UTF8&language=en_US&node=1094656") + ", enjoy the HTML format.")
 	else:
-		return None
-
-
-print(googleData())
+		return True
+	# To engage with my inner creativity, it will not be that creative, but; who cares! XD 
+	try:
+		except amazonUrl as True:
+			if amazonUrl == True:
+				with amazonUrl:
+					# It will execute the overall HTML format of Amazon
+					# Only works HTTPS domain, apparently...
+					result = resp.content(["Content-Type"].lower())
+					# Calling the HTML format, therefore, this program maybe; further advanced
+					return (result == 100 or result is not None > -1)
+			else:
+				# When the amazon URL is 
+				result = resp.content("html.parser", url)
+				amazonUrl =False
+				return result
